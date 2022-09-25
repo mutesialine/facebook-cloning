6242526566ce6e525715e5ec2518a29bbdc71501
Mutesi <ktesialine@gmail.com>
2022-09-25 22:29:16 +0200

    updated design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "1900px",
+      },
+    },
   },
   plugins: [],
 };
